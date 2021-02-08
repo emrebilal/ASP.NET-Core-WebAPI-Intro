53315ff7be22556d36efc0cd32bf20d11cf39f31
Emre Bilal <emre_bilal21@hotmail.com>
2021-02-08 20:39:28 +0300

    Added files

---
+++ WebAPI_Intro/Controller/ProductsController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_Intro.Data;
+using WebAPI_Intro.Model;
+
+namespace WebAPI_Intro.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataManager dataManager;
+        public ProductsController()
+        {
+            dataManager = DataManager.Instance;
+        }
+
+        [HttpGet("/api/products")]
+        public List<ProductModel> GetProducts()
+        {
+            return dataManager.Products;
+        }
+
+        [HttpGet("/api/products/{id}")]
+        public ProductModel GetProduct(int id)
+        {
+            var data = dataManager.Products.FirstOrDefault(c => c.Id == id);
+
+            return data;
+        }
+
+        [HttpPost("/api/products")]
+        public ProductModel AddProduct([FromBody] ProductModel product)
+        {
+            dataManager.Products.Add(product);
+            dataManager.SaveJsonData();
+
+            return product;
+        }
+
+        [HttpPut("/api/products/{id}")]
+        public void UpdateProduct(int id, [FromBody] ProductModel product)
+        {
+            for (var index = dataManager.Products.Count - 1; index >= 0; index--)
+            {
+                if (dataManager.Products[index].Id == id)
+                {
+                    dataManager.Products[index] = product;
+                }
+            }
+            dataManager.SaveJsonData();
+        }
+
+        [HttpDelete("/api/products/{id}")]
+        public void DeleteProduct(int id)
+        {
+            for (var index = dataManager.Products.Count - 1; index >= 0; index--)
+            {
+                if (dataManager.Products[index].Id == id)
+                {
+                    dataManager.Products.RemoveAt(index);
+                }
+            }
+            dataManager.SaveJsonData();
+        }
+
+    }
+}
---
+++ WebAPI_Intro/Data/DataManager.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_Intro.Model;
+
+namespace WebAPI_Intro.Data
+{
+    public class DataManager
+    {
+        private static readonly DataManager _instance;
+
+        static DataManager()
+        {
+            _instance = new DataManager();
+        }
+
+        private DataManager()
+        {
+            FillData();
+        }
+
+        public static DataManager Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public List<ProductModel> Products = new List<ProductModel>();
+
+        public void FillData()
+        {
+            string data = File.ReadAllText("sampledata.json");
+            List<ProductModel> tempProducts = JsonConvert.DeserializeObject<List<ProductModel>>(data);
+
+            if (tempProducts != null)
+            {
+                foreach (var product in tempProducts)
+                {
+                    Products.Add(new ProductModel { Id = product.Id, ProductName = product.ProductName, ProductCategory = product.ProductCategory, Price = product.Price });
+                }
+            }
+        }
+
+        public void SaveJsonData()
+        {
+            string newData = JsonConvert.SerializeObject(Products, Formatting.Indented);
+            File.WriteAllText("sampledata.json", newData);
+        }
+    }
+}
